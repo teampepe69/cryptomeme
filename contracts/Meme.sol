75c37661e1544ae788dc42ce269f064b64da076f
unknown <quentinkhoo@outlook.com>
2020-03-14 02:33:06 +0800

    Fix Meme.sol

--- contracts/Meme.sol
+++ contracts/Meme.sol
@@ -11,19 +11,24 @@ contract Meme is ERC721 {
     enum memeStates {approved, rejected, pending}
 
     struct Meme {
-        uint256 numLikes,
-        string memory memePath,
-        memeStates memeState
+        uint256 numLikes;
+        string memePath;
+        memeStates memeState;
     }
 
     Meme[] memes;
 
-    event memeCreated(uint256 memeId, string memory memePath);
+    event memeCreated(uint256 memeId, string memePath);
 
-    function createMeme(address _memeOwner, string memory _pathToMeme) {
-        Meme memory _meme = meme(numberOfMemes, 0, _pathToMeme, memeState.pending);
+    function createMeme(address _memeOwner, string memory _pathToMeme) public {
+        Meme memory _meme = Meme(
+            numberOfMemes,
+            _pathToMeme,
+            memeStates.pending
+        );
         uint256 _memeId = memes.push(_meme).sub(1);
         _mint(_memeOwner, _memeId);
+        numberOfMemes = numberOfMemes.add(1);
         emit memeCreated(_memeId, _pathToMeme);
     }
 
@@ -31,27 +36,27 @@ contract Meme is ERC721 {
         return memes[_memeId].numLikes;
     }
 
-    function getMemeState(uint256 _memeId) public view returns {
+    function getMemeState(uint256 _memeId) public view returns (memeStates) {
         return memes[_memeId].memeState;
     }
 
-    function getMemePath(uint256 _memeId) {
+    function getMemePath(uint256 _memeId) public view returns (string memory) {
         return memes[_memeId].memePath;
     }
 
     function setMemeLikes(uint256 _memeId, uint256 _memeLikes) public {
-        memes[_memeId].memeLikes = _memeLikes;
+        memes[_memeId].numLikes = _memeLikes;
     }
 
-    function approveMeme(uint256 _memeId) {
+    function approveMeme(uint256 _memeId) public {
         memes[_memeId].memeState = memeStates.approved;
-    } 
-
-    function rejectMeme(uint256 _memeId) {
-        memes[_memeId].memestate = memeStates.rejected;
     }
 
-    function setMemePath(uint256 _memeId, string memory _memePath) {
+    function rejectMeme(uint256 _memeId) public {
+        memes[_memeId].memeState = memeStates.rejected;
+    }
+
+    function setMemePath(uint256 _memeId, string memory _memePath) public {
         memes[_memeId].memePath = _memePath;
     }
 
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,7 @@
 var SimpleStorage = artifacts.require("./SimpleStorage.sol");
+var Meme = artifacts.require("./Meme.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(SimpleStorage);
+  deployer.deploy(Meme);
 };

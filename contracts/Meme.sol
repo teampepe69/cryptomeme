9445d6f101a3f6bbb622a5b0b231fc320a319f62
unknown <quentinkhoo@outlook.com>
2020-03-14 04:31:52 +0800

    Add test for Meme.sol

--- contracts/Meme.sol
+++ contracts/Meme.sol
@@ -18,9 +18,16 @@ contract Meme is ERC721 {
 
     Meme[] memes;
 
-    event memeCreated(uint256 memeId, string memePath);
+    event MemeCreated(uint256 memeId, string memePath);
+    event MemeLiked(uint256 memeId, uint256 memeLikes);
+    event MemeApproved(uint256 memeId);
+    event MemeRejected(uint256 memeId);
+    event MemePathChanged(uint256 memeId, string memePath);
 
-    function createMeme(address _memeOwner, string memory _pathToMeme) public {
+    function createMeme(address _memeOwner, string memory _pathToMeme)
+        public
+        returns (uint256)
+    {
         Meme memory _meme = Meme(
             numberOfMemes,
             _pathToMeme,
@@ -29,10 +36,11 @@ contract Meme is ERC721 {
         uint256 _memeId = memes.push(_meme).sub(1);
         _mint(_memeOwner, _memeId);
         numberOfMemes = numberOfMemes.add(1);
-        emit memeCreated(_memeId, _pathToMeme);
+        emit MemeCreated(_memeId, _pathToMeme);
+        return _memeId;
     }
 
-    function getNumberLikes(uint256 _memeId) public view returns (uint256) {
+    function getMemeLikes(uint256 _memeId) public view returns (uint256) {
         return memes[_memeId].numLikes;
     }
 
@@ -46,18 +54,22 @@ contract Meme is ERC721 {
 
     function setMemeLikes(uint256 _memeId, uint256 _memeLikes) public {
         memes[_memeId].numLikes = _memeLikes;
+        emit MemeLiked(_memeId, _memeLikes);
     }
 
     function approveMeme(uint256 _memeId) public {
         memes[_memeId].memeState = memeStates.approved;
+        emit MemeApproved(_memeId);
     }
 
     function rejectMeme(uint256 _memeId) public {
         memes[_memeId].memeState = memeStates.rejected;
+        emit MemeRejected(_memeId);
     }
 
     function setMemePath(uint256 _memeId, string memory _memePath) public {
         memes[_memeId].memePath = _memePath;
+        emit MemePathChanged(_memeId, _memePath);
     }
 
 }

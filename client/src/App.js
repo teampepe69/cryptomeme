82b1d2624e395f28687d3362eba6f02859fa5628
TzeMeng <TzeMeng@users.noreply.github.com>
2020-03-23 16:48:08 +0800

    moving bar

--- client/src/App.js
+++ client/src/App.js
@@ -11,29 +11,54 @@ import {
   NavLink
 } from "react-router-dom";
 import LandingPage from './Landing/pages/LandingPage.js'
-import Subbar from './Landing/pages/Subbar.js'
 import Navbar from './Navbar/pages/Navbar.js'
 
+// import Subbar from './Subbar/pages/Subbar'
+import SideDrawer from './Subbar/components/SideDrawer'
+// import Backdrop from './Subbar/components/Backdrop'
+
 class App extends Component {
+  // state = {
+  //   sideDrawerOpen: false,
+  // }
+
+  // drawerToggleClickHandler = () => {
+  //   this.setState(prevState => {
+  //     return { sideDrawerOpen: !prevState.sideDrawerOpen }
+  //   })
+  // }
+
+  // backdropClickHandler = () => {
+  //   this.setState({ sideDrawerOpen: false })
+  // }
+
   render() {
-    
+    // let backdrop;
+
+    // if (this.state.sideDrawerOpen) {
+    //   backdrop = <Backdrop click={this.backdropClickHandler} />;
+    // }
     return (
       <div style={{backgroundColor:'#9acdbaff', height: '100%', minHeight: '100vh'}}>
         <Router>
           <Navbar />
             <div style={{paddingTop: '100px'}} >
-              <div style={{paddingLeft: '20px'}}>
-                <Subbar />
+              <div style={{paddingLeft: '20px'}}></div>
+
+              <SideDrawer />
+
+              {/* <Subbar drawerClickHandler={this.drawerToggleClickHandler} />
+              <SideDrawer show={this.state.sideDrawerOpen} />
+              {backdrop} */}
               </div>
               <div style={{paddingLeft: '200px'}}>
                 <Route exact path="/" component={LandingPage} />
               </div>
-              
-            </div>
         </Router>
+
       </div>
     )
   }
 }
 
-export default App;
+export default App

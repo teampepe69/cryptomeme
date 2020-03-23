e8c3a41d8c6de5077fdcd774535ebfac42afd88f
TzeMeng <TzeMeng@users.noreply.github.com>
2020-03-23 16:50:07 +0800

    Static subbar

--- client/src/App.js
+++ client/src/App.js
@@ -13,43 +13,17 @@ import {
 import LandingPage from './Landing/pages/LandingPage.js'
 import Navbar from './Navbar/pages/Navbar.js'
 
-// import Subbar from './Subbar/pages/Subbar'
 import SideDrawer from './Subbar/components/SideDrawer'
-// import Backdrop from './Subbar/components/Backdrop'
 
 class App extends Component {
-  // state = {
-  //   sideDrawerOpen: false,
-  // }
-
-  // drawerToggleClickHandler = () => {
-  //   this.setState(prevState => {
-  //     return { sideDrawerOpen: !prevState.sideDrawerOpen }
-  //   })
-  // }
-
-  // backdropClickHandler = () => {
-  //   this.setState({ sideDrawerOpen: false })
-  // }
-
   render() {
-    // let backdrop;
-
-    // if (this.state.sideDrawerOpen) {
-    //   backdrop = <Backdrop click={this.backdropClickHandler} />;
-    // }
     return (
       <div style={{backgroundColor:'#9acdbaff', height: '100%', minHeight: '100vh'}}>
         <Router>
           <Navbar />
             <div style={{paddingTop: '100px'}} >
               <div style={{paddingLeft: '20px'}}></div>
-
               <SideDrawer />
-
-              {/* <Subbar drawerClickHandler={this.drawerToggleClickHandler} />
-              <SideDrawer show={this.state.sideDrawerOpen} />
-              {backdrop} */}
               </div>
               <div style={{paddingLeft: '200px'}}>
                 <Route exact path="/" component={LandingPage} />

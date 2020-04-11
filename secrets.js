b4900a5e4a8fc086f449d7a480e60ee9dda629d2
Quentin Khoo <quentinkhoo@outlook.com>
2020-04-12 01:36:46 +0800

    Add files for ropsten

---
+++ secrets.js
@@ -0,0 +1,11 @@
+var projectId = "16923192f0ca478ab86d2123538206bd";
+var mnemonic =
+  "lawn front sustain yard super review bronze put duty thumb model time";
+var ropstenPK =
+  "EEB6B17623C185990F2456CD6A54102B4E0EC6C36CF88E556015AB06232B1A1F";
+
+module.exports = {
+  projectId: projectId,
+  ropstenPk: ropstenPk,
+  mnemonic: mnemonic,
+};
--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,12 @@
-const path = require("path");
+const secrets = require("./secrets.js");
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+let ropstenPrivateKey = new Buffer(secrets.ropstenPK, "hex");
+let ropstenWallet = Wallet.fromPrivateKey(ropstenPrivateKey);
+let ropstenProvider = new WalletProvider(
+  ropstenWallet,
+  "https://ropsten.infura.io/"
+);
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -8,8 +16,12 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
-    }
+      network_id: "*",
+    },
+    ropsten: {
+      provider: ropstenProvider,
+      network_id: "3",
+    },
   },
-  plugins: ["solidity-coverage"]
+  plugins: ["solidity-coverage"],
 };
